770f6041d6ec5dfe19f04e6e0d26e0ea21b7e594
luizanac <luizantoniojj@hotmail.com>
2016-10-28 17:12:35 -0200

    up

--- application/views/adm/loginView.php
+++ application/views/adm/loginView.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/materialize/css/materialize.min.css');?> ">
 	<link rel="stylesheet" type="text/css" href="<?= base_url('assets/materialize/css/style.css');?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
